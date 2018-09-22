61f2a1249061844613165b53e8c79f919c6cdb0c
Johan Martinez <jomarquin@gmail.com>
2018-09-21 23:36:24 -0500

    Subiendo codigo Lab2Activities
    Subiendo codigo Lab2Activities

---
+++ build/generated/source/buildConfig/debug/co/edu/udea/compumovil/gr01_20182/lab2/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package co.edu.udea.compumovil.gr01_20182.lab2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "co.edu.udea.compumovil.gr01_20182.lab2";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
